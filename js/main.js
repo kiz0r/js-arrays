52c2b216748921007ebe09730c219c2804f77274
Kirill Khomenko <kkhomenko10@gmail.com>
2022-11-21 10:40:26 +0100

    make task 11

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
     - use .shift() when you want to delete the first element of your array
     - use .pop() when you want to delete the last element of your array
     - use .unshift() when you want to add the last element to your array
-    - use .push() when you want to add the first element to your array use  
+    - use .push() when you want to add the first element to your array use
 */
 
 const arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -41,6 +41,43 @@ console.log('The third copy of arrayOfNumbers : ', arrayOfNumbers3);
 // 6 Вивести індекси нульових елементів (елемент дорівнює нулю).
 // 7 Підрахувати кількість нульових елементів.
 
+console.group('Output elements of array with paired index : ');
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+  if (i % 2 === 0) {
+    console.log(arrayOfNumbers[i]);
+  }
+}
+console.groupEnd();
+
+console.group('Output paired elements of array : ');
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+  if (arrayOfNumbers[i] % 2 === 0) {
+    console.log(arrayOfNumbers[i]);
+  }
+}
+console.groupEnd();
+
+// array with zeros
+const array1 = [-7, 5, 0, 1, 0, -2, 0, 6, 8, 0, 11, 0, 0];
+
+console.group('Indexes, which consist 0 ');
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] === 0) {
+    console.log('index, which consists 0 : ' + i);
+  }
+}
+console.groupEnd();
+
+let sumOfIndexes = 0;
+
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] === 0) {
+    sumOfIndexes++;
+  }
+}
+
+console.log('Sum of zero elements = ', sumOfIndexes);
+
 // Методи перебору масивів (forEach, filter, map, findIndex, *some, *every).
 // 8 Отримати новий масив із заданого, який міститиме лише ненульові числа (-1, 5, 0, 9, -10 => -1, 5, 9, -10).
 // 9 Отримати новий масив їх заданого, який міститиме всі елементи вихідного, поділені на 100 (99, 5, 0, 9, 30 => 0.99, 0.05, 0, 0.09, 0.3).
@@ -48,3 +85,64 @@ console.log('The third copy of arrayOfNumbers : ', arrayOfNumbers3);
 // 11 Визначити індекс елемента, квадрат якого дорівнює 100, і видалити його, або видати діагностичне повідомлення, якщо такого елементу не існує.
 // 12 *Перевірити, чи всі елементи масиву є парними числами (* або простими числами).
 // 13 *Перевірити, чи є у масиві бодай один від'ємний елемент.
+
+// delete all zeros in array
+const unformattedArray = [-1, 5, 0, 9, -10];
+
+const formattedArray = unformattedArray.filter(
+  (currentValue) => currentValue !== 0
+);
+
+console.log('Formatted array without zeros : ', formattedArray);
+
+// divide all elements of array by 100
+const needToDivideArray = [99, 5, 0, 9, 30];
+
+function divideBy100(currentValue) {
+  return currentValue / 100;
+}
+
+const dividedArray = needToDivideArray.map(divideBy100);
+
+console.log('array which was divided by 100 : ', dividedArray);
+
+// output all elements of the array in the third power
+function toThirdPower(currentValue, index, array) {
+  console.log(`${array[index]}^3 = ${currentValue ** 3}`);
+}
+
+console.group('All elements of array in the third power : ');
+unformattedArray.forEach(toThirdPower);
+console.groupEnd();
+
+// delete element of array, square of which is 100 or output message 'there is no element square of which is 100'
+const find10 = (item) => item ** 2 === 100;
+
+function deleteElementFromArray(array, indexToDelete) {
+  if (indexToDelete !== -1) {
+    array.splice(indexToDelete, 1);
+    return 'Element was successfully deleted.';
+  } else {
+    return 'There is no element to delete.';
+  }
+}
+
+const thereIs100 = [-8, 0, 5, -2, 10, 11, -10];
+
+let foundIndex = thereIs100.findIndex(find10);
+
+console.log(
+  deleteElementFromArray(thereIs100, foundIndex),
+  'Array : ',
+  thereIs100
+);
+
+const thereIsNot100 = [-5, 7, 99, 3, 0, -2, 1];
+
+foundIndex = thereIsNot100.findIndex(find10);
+
+console.log(
+  deleteElementFromArray(thereIsNot100, foundIndex),
+  'Array : ',
+  thereIsNot100
+);
